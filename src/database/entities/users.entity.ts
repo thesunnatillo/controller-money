14b632958828baa0156c070dff63a766acb7e636
thesunnatillo <sunnatillocoder@gmail.com>
2025-06-17 16:29:52 +0500

    ref: clone template

--- src/database/entities/users.entity.ts
+++ src/database/entities/users.entity.ts
@@ -1,6 +1,5 @@
 import { Column, Entity, JoinColumn, OneToOne } from "typeorm";
 import { GeneralEntity } from "../../utils/entity/base.entity";
-import { PostsEntity } from "./posts.entity";
 
 @Entity({ name: "users", schema: "public" })
 export class UsersEntity extends GeneralEntity {
@@ -14,9 +13,4 @@ export class UsersEntity extends GeneralEntity {
     @Column("varchar", { name: "username", nullable: true })
     username: string;
 
-    @OneToOne(() => PostsEntity )
-    @JoinColumn({
-        name: "post_id"
-    })
-    post: PostsEntity;
 }
